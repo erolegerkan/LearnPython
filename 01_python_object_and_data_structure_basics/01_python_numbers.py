76850d63c350c125d9e2bd45f83701dd8e182955
Erol Ege ERKAN <erolegeerkan@Erol-MacBook-Pro.local>
2025-02-12 20:50:44 +0300

    Initial commit

---
+++ 01_python_object_and_data_structure_basics/01_python_numbers.py
@@ -0,0 +1,20 @@
+# To take an exponential of a number
+#print(2**3) #8
+
+#print(0.1+0.2-0.3) #5.551115123125783e-17
+
+#Computers store numbers in binary (base 2), 
+# but some decimal fractions, like 0.1 and 0.2,
+#  do not have exact binary representations. 
+# Instead, they are approximated, leading to small rounding errors.
+# To handle this kind of issues, round() function often use by programmers.
+
+# Python has 'dynamic typing' - This cause same pros and also some cons. 
+
+a = 5
+print(a)
+print(type(a))
+
+a = 'Hello'
+print(a)
+print(type(a))
---
+++ 01_python_object_and_data_structure_basics/02_python_strings.py
@@ -0,0 +1,21 @@
+# when declaring string,you can use both single or double quotes
+
+#print("I'am going on a run")
+
+#print(len('hello'))
+
+# mystring = 'Hello World'
+# print(mystring[0])
+# print(mystring[8]) #indexing
+# print(mystring[-2]) #reverse indexing
+
+# mystring = "abcdefghijk"
+# print(mystring[2])
+# print(mystring[2:]) # slicing from index 2 to end of the string
+# print(mystring[:3]) # index 3 ('d') is not included in the slice
+# print(mystring[3:6]) # 'def'
+# print(mystring[::]) # it takes all string - all the way from beginning to end with stepsize 1 (default)
+# print(mystring[::2]) # all the way from beginning to end with stepsize 2
+# # [start:end:stepsize]
+# print(mystring[::-1]) # reverse string - good trick :)
+
