d222e45e5b96979bad6fd6deed7e67187eb7fbad
erolegerkan <erolege.education@hotmail.com>
2025-02-16 11:49:38 +0300

    Strings

--- 01_python_object_and_data_structure_basics/02_python_strings.py
+++ 01_python_object_and_data_structure_basics/02_python_strings.py
@@ -19,3 +19,51 @@
 # # [start:end:stepsize]
 # print(mystring[::-1]) # reverse string - good trick :)
 
+#Immutability
+# name = "Sam"
+# #name[0] = 'E' #TypeError: 'str' object does not support item assignment
+# last_letters = name[1:]
+# print('P' + last_letters) #String concatenation
+
+# letter = 'z'
+# print(letter*10) # String multiplication
+
+# x = 'Hello World'
+# print(x.upper()) #it does not change the original one
+# print(x)
+# x = x.upper() #now original one is changed
+# print(x)
+# print(x.lower())
+# print(x.split())
+
+# x = 'Hi this is a string'
+# print(x.split('i')) # split according to given character
+# #split() -> create lists
+# x = "This is a string {}"
+# print("This is a string {}".format('INSERTED'))
+# print(x.format('INSERTED')) # it does not insert the original string
+# print(x)
+
+# print('The {2} {1} {0}'.format('brown','fox','quick'))
+# # 'The quick fox brown'
+# print('The {0} {0} {0}'.format('brown','fox','quick'))
+# # 'The fox fox fox'
+# print('The {one} {two} {three}'.format(one='brown',two='fox',three='quick'))
+# # 'The brown fox quick'
+
+# result = 100 / 177 
+# print(result) # 0.5649717514124294
+# print("The result was {r:1.3f}".format(r=result)) # 0.565
+
+# new_result = 104.12345
+# print('the result was {r:1.2f}'.format(r=result))
+# # 104.52
+
+# name = 'Ege'
+# print(f'Hello, my name is {name}')
+# # Hello, my name is Ege
+
+# name = 'Sam'
+# age = 3
+# print(f'{name} is {age} years old')
+
